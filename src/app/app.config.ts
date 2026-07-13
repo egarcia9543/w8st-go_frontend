dce6263ea83c6c7425208672cc4fb55645712e4c
egarcia9543 <egarciadev420@gmail.com>
2026-07-13 01:02:03 -0500

    feat(infrastructure): add credentials interceptor and register providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { credentialsInterceptor } from './infrastructure/interceptors/credentials-interceptor';
+import { provideSpartanHlm } from '@spartan-ng/helm/utils';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([credentialsInterceptor])
+    ),
+    provideSpartanHlm()
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://w8st-go-backend.vercel.app',
+};
